a8a160e8f4d5dfc343ff1dfd19e3456906ddbc71
Matt Kocubinski <mkocubinski@gmail.com>
2019-04-02 21:22:25 -0400

    Tooling for testing.

--- dev/dev.clj
+++ dev/dev.clj
@@ -20,16 +20,64 @@
    [clojure.tools.namespace.repl :refer [refresh refresh-all clear]]
    [com.stuartsierra.component :as component]
    [com.stuartsierra.component.repl :refer [reset set-init start stop system]]
-   [org.purefn.river]))
+   [org.purefn.kurosawa.log.core :as klog]
+   [org.purefn.river :as river]
+   [org.purefn.river.serdes.nippy :as serdes])
+  (:import [java.util UUID]
+           [org.apache.kafka.clients.producer KafkaProducer ProducerRecord]))
 
 ;; Do not try to load source code from 'resources' directory
 (clojure.tools.namespace.repl/set-refresh-dirs "dev" "src" "test")
+
+;;--------------------------------------------------------------------------------
+;; Kafka Producer
+
+(defrecord Producer
+    [producer config]
+
+  component/Lifecycle
+  (start [this]
+    (let [{:keys [config producer]} this]
+      (if producer
+        this
+        (assoc this :producer (KafkaProducer. config
+                                              (serdes/nippy-serializer)
+                                              (serdes/nippy-serializer))))))
+
+  (stop [this]
+    (.close producer)
+    (assoc this :producer nil)))
+
+(defn producer
+  []
+  (map->Producer
+   {:config {"bootstrap.servers"  "localhost:9092"
+             "client.id"          (str (UUID/randomUUID))
+             "acks"               "all"
+             "retries"            (Integer. 0)
+             "batch.size"         (Integer. 16384)
+             "linger.ms"          1
+             "max.block.ms"       (Integer. 1000)
+             "request.timeout.ms" (Integer. 1000)
+             "compression.type"   "none"
+             "buffer.memory"      33554432}}))
+
+(defn send-record
+  [^Producer producer topic key value]
+  (.send (:producer producer) (ProducerRecord. topic key value)))
+
+;;--------------------------------------------------------------------------------
+;; System
+
 
 (defn dev-system
   "Constructs a system map suitable for interactive development."
   []
   (component/system-map
-   ;; TODO
+   :producer (producer)
    ))
 
-(set-init (fn [_] (dev-system)))
+(set-init (fn [_] (let [sys (dev-system)]
+                    (klog/init-dev-logging sys)
+                    sys)))
+
