b95eef90e0097de90ca7cef81b0f62e58cb16c67
Matt Kocubinski <mkocubinski@gmail.com>
2019-04-29 14:36:08 -0400

    Add deploy repo.

--- project.clj
+++ project.clj
@@ -15,6 +15,9 @@
   
   :java-source-paths ["java"]
 
+  :deploy-repositories
+  [["releases" {:url "https://clojars.org/repo/" :creds :gpg}]]
+
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.11"]
                                   [org.purefn/kurosawa.aws "2.1.4"]
                                   [com.stuartsierra/component.repl "0.2.0"]
