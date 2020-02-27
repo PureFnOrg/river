e2bfd4e34f6ede055e6459569b7575eb59d55d3d
Matt Kocubinski <mkocubinski@gmail.com>
2020-02-26 20:41:15 -0500

    Version 0.1.3-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.purefn/river "0.1.2"
+(defproject org.purefn/river "0.1.3-SNAPSHOT"
 
   :description "Minimalist Consumers for Kafka Topics."
   :url "https://github.com/purefnorg/river"
